d547fd6c3592018e6079e73e4550bc2430323092
subin <tvsu0808@gmail.com>
2025-02-18 16:11:51 +0900

    0218

---
+++ README.md
@@ -0,0 +1,8 @@
+# python
+## 가상환경
+
+- `python -m venv venv` : 가상환경 생성
+- `source venv/Scripts/activate` : 가상환경 활성화
+    - linux, mac의 경우 : `Scripts` 대신 `bin`
+- `deactivate` : 가상환경 비활성화
+
---
+++ myPackage/math/fomula.py
@@ -0,0 +1,14 @@
+pi = 3.14
+
+def my_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return fact(n-1) * 1
+
